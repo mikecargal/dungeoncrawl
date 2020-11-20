9ed9deee0d8154c26b7f09b2ca9333835af0f0b8
Mike Cargal <mike@cargal.net>
2020-11-20 16:43:40 -0500

    clean  up comments

--- src/main.rs
+++ src/main.rs
@@ -52,12 +52,12 @@ impl GameState for State {
 fn main() -> BError {
     let context = BTermBuilder::new()
         .with_title("Dungeon Crawler")
-        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT) // (1)
-        .with_tile_dimensions(32, 32) // (2)
-        .with_resource_path("resources/") // (3)
-        .with_font("dungeonfont.png", 32, 32) // (4)
-        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (5)
-        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (6)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())
