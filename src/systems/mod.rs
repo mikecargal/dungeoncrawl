10ab14517dfc2dc9287db71b423f6cc6da4bc622
Mike Cargal <mike@cargal.net>
2020-11-23 12:04:07 -0500

    Turn based

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,19 +2,51 @@ mod entity_render;
 mod map_render;
 // mod player_coord_render;
 mod collisions;
+mod end_turn;
 mod player_input;
 mod random_moves;
 
 use crate::prelude::*;
 
-pub fn build_scheduler() -> Schedule {
+pub fn build_input_schedule() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
+        .build()
+}
+
+pub fn build_player_schedule() -> Schedule {
+    Schedule::builder()
         .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
-        .add_system(random_moves::random_move_system())
-        // .add_system(player_coord_render::player_coord_render_system())
+        .add_system(end_turn::end_turn_system())
         .build()
 }
+
+pub fn build_monster_schedule() -> Schedule {
+    Schedule::builder()
+        .add_system(random_moves::random_move_system())
+        .flush()
+        .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
+        .add_system(end_turn::end_turn_system())
+        .build()
+}
+
+// pub fn build_scheduler() -> Schedule {
+//     Schedule::builder()
+//         .add_system(player_input::player_input_system())
+//         .add_system(collisions::collisions_system())
+//         .flush()
+//         .add_system(map_render::map_render_system())
+//         .add_system(entity_render::entity_render_system())
+//         .add_system(random_moves::random_move_system())
+//         // .add_system(player_coord_render::player_coord_render_system())
+//         .build()
+// }
