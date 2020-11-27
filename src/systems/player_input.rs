4de3293a1605bafe82855e07469217a06b31c877
Mike Cargal <mike@cargal.net>
2020-11-27 15:03:15 -0500

    waiting

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,18 +3,21 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
     #[resource] turn_state: &mut TurnState,
 ) {
-    if turn_state.clone() != TurnState::AwaitingInput {
-        return;
-    }
+    // if turn_state.clone() != TurnState::AwaitingInput {
+    //     return;
+    // }
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+    let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+
     if let Some(key) = key {
-        let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
-        let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
             VirtualKeyCode::Right => Point::new(1, 0),
@@ -22,17 +25,19 @@ pub fn player_input(
             VirtualKeyCode::Down => Point::new(0, 1),
             _ => Point::new(0, 0),
         };
+        let mut did_something = false;
+        let (player_entity, destination) = players
+            .iter(ecs)
+            .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
+            .unwrap();
         if delta.x != 0 || delta.y != 0 {
-            let (player_entity, destination) = players
-                .iter(ecs)
-                .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
-                .unwrap();
             let mut hit_something = false;
             enemies
                 .iter(ecs)
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -43,6 +48,7 @@ pub fn player_input(
                 });
 
             if !hit_something {
+                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -51,8 +57,17 @@ pub fn player_input(
                     },
                 ));
             }
-
-            *turn_state = TurnState::PlayerTurn;
         }
+
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current + 1);
+            }
+        }
+        *turn_state = TurnState::PlayerTurn;
     }
 }
