617844c8a6471ec10cb44cde8f0fd132a498a87c
Mike Cargal <mike@cargal.net>
2020-12-27 12:24:13 -0500

    Green-Yellow-Red HUD health display

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -11,12 +11,17 @@ pub fn hud(ecs: &SubWorld, #[resource] map: &Map) {
     draw_batch.target(HUD_LAYER.id);
     draw_batch.print_centered(0, "Explore the Dungeon.  Cursor keys to move.");
     let health_x = (map.height - 1) * 2;
+    let health_color = match player_health.current {
+        h if h > 7 => GREEN,
+        h if h > 3 => YELLOW,
+        _ => RED,
+    };
     draw_batch.bar_horizontal(
         Point::new(0, health_x),
         map.width * 2,
         player_health.current,
         player_health.max,
-        ColorPair::new(RED, BLACK),
+        ColorPair::new(health_color, BLACK),
     );
     draw_batch.print_color_centered(
         health_x,
@@ -24,7 +29,7 @@ pub fn hud(ecs: &SubWorld, #[resource] map: &Map) {
             " Health: {} / {} ",
             player_health.current, player_health.max
         ),
-        ColorPair::new(WHITE, RED),
+        ColorPair::new(WHITE, health_color),
     );
     draw_batch.submit(HUD_LAYER.z_order).expect("Batch error");
 }
