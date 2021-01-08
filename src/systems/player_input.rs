8997b8566deaee78921b3ddf707535ae51f51478
Mike Cargal <mike@cargal.net>
2021-01-08 15:36:50 -0500

    remove wait to heal (End of Chapter 13)

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -79,7 +79,6 @@ pub fn player_input(
                 });
 
             if !hit_something {
-                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -90,15 +89,6 @@ pub fn player_input(
             }
         }
 
-        if !did_something {
-            if let Ok(mut health) = ecs
-                .entry_mut(player_entity)
-                .unwrap()
-                .get_component_mut::<Health>()
-            {
-                health.current = i32::min(health.max, health.current + 1);
-            }
-        }
         *turn_state = TurnState::PlayerTurn;
     }
 }
