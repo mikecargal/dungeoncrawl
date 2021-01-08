d4a54f7fa9f5d8258edeb4c0b1e160ab95cea547
Mike Cargal <mike@cargal.net>
2021-01-08 17:20:50 -0500

    Level in Hub (End of Chapter 14)

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -36,22 +36,38 @@ pub fn hud(ecs: &SubWorld, #[resource] map: &Map) {
         ColorPair::new(WHITE, health_color),
     );
 
+    let (_player, map_level) = <(Entity, &Player)>::query()
+        .iter(ecs)
+        .find_map(|(entity, player)| Some((*entity, player.map_level)))
+        .unwrap();
+
+    draw_batch.print_color_right(
+        Point::new(map.width * 2, 1),
+        format!("Dungeon Level: {}", map_level + 1),
+        ColorPair::new(YELLOW, BLACK),
+    );
+
     let player = <(Entity, &Player)>::query()
         .iter(ecs)
         .find_map(|(entity, _player)| Some(*entity))
         .unwrap();
     let mut item_query = <(&Item, &Name, &Carried)>::query();
-    let mut y = 3;
+    let item_top_line = 2;
+    let item_column = 3;
+    let mut y = item_top_line;
     item_query
         .iter(ecs)
         .filter(|(_item, _name, carried)| carried.by == player)
         .for_each(|(_item, name, _carried)| {
-            draw_batch.print(Point::new(3, y), format!("{} : {}", y - 2, &name.0));
             y += 1;
+            draw_batch.print(
+                Point::new(item_column, y),
+                format!("{} : {}", y - 2, &name.0),
+            );
         });
-    if y > 3 {
+    if y > item_top_line {
         draw_batch.print_color(
-            Point::new(3, 2),
+            Point::new(item_column, item_top_line),
             "Items carried",
             ColorPair::new(YELLOW, BLACK),
         );
